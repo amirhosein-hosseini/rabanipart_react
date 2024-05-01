a16e6e9c89e37ee41698795fd2e76e3562b61f19
amirhossein <hoseinia0870@gmail.com>
2024-05-01 12:10:54 +0330

    last computer update

--- src/components/gallery/imageGallery.js
+++ src/components/gallery/imageGallery.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
-import gallery1image from "../../asset/image/galleryimage.png"
-import gallery1image2 from "../../asset/image/indexcontactrightimage.png"
-import gallery1image3 from "../../asset/image/shopitemimage.png"
+import gallery1image from "../../asset/image/galleryimage.jpg"
 import "./galleryStyle.css"
 
 
@@ -13,12 +11,12 @@ const images = [
     thumbnail: gallery1image,
   },
   {
-    original: gallery1image2,
-    thumbnail: gallery1image2,
+    original: gallery1image,
+    thumbnail: gallery1image,
   },
   {
-    original: gallery1image3,
-    thumbnail: gallery1image3,
+    original: gallery1image,
+    thumbnail: gallery1image,
   },
   // Add more images as needed
 ];
